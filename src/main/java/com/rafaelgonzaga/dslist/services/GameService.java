789cdde076e6cab95a4fe6e2bf48d8bd5b9043ea
Rafael Gonzaga <ralozaga@gmail.com>
2023-05-11 13:47:51 -0300

    GET game by id

--- src/main/java/com/rafaelgonzaga/dslist/services/GameService.java
+++ src/main/java/com/rafaelgonzaga/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rafaelgonzaga.dslist.dto.GameDTO;
 import com.rafaelgonzaga.dslist.dto.GameMinDTO;
 import com.rafaelgonzaga.dslist.entities.Game;
 import com.rafaelgonzaga.dslist.repositories.GameRepository;
@@ -15,10 +17,19 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true) // Não bloqueia o BD para escrita
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
+	
 
 }
